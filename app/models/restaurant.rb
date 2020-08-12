608c371ac19476a8d2775c970be4b44586158394
Lea Bugeat <lbugeat@gmail.com>
2020-08-12 12:54:18 +0200

    complete Yelp MVP

---
+++ app/models/restaurant.rb
@@ -0,0 +1,7 @@
+class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+
+  validates :category, inclusion: { in: %w(chinese italian japanese french belgian) }
+
+  validates :name, :address, presence: true
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+maisondufleuve = { name: "La maison du fleuve", address: "Camblanes et Meynac", phone_number: "O5 02 03 04 06", category: "french" }
+risotto =  { name: "Le risotto", address: "Merignac", phone_number: "O1 23 45 67 89", category: "italian" }
+friterie = { name: "La Friterie", address: "Bruxelles", phone_number: "O1 98 45 67 32", category: "belgian" }
+royalbuffet = { name: "Royal Buffet", address: "Merignac", phone_number: "O1 23 46 65 89", category: "chinese" }
+sushishop = { name: "sushishop", address: "Bordeaux", phone_number: "O1 25 45 77 89", category: "japanese" }
+
+[ maisondufleuve, risotto, friterie, royalbuffet, sushishop ].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
